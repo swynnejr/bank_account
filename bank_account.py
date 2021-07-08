abe6c6dcbe05d3bb76f256ea6f041f9c1f98f238
swynnejr <saucersam@gmail.com>
2021-07-08 17:25:42 -0500

    starting fresh typed not copied

---
+++ bank_account.py
@@ -0,0 +1,10 @@
+class BankAccount:
+    def __init__(self, int_rate, balance):
+
+    def deposit(self, amount):
+
+    def withdrawl(self, amount):
+
+    def display_account_info(self):
+
+    def yield_interest(self):
